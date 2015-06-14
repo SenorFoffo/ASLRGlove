25ea172e168cadc78c5a9ef36fec97d76926a4a9
andrelutza <anio1491@yahoo.co.uk>
2015-06-14 18:53:51 +0200

    Update tcp_server.m

--- src/tcp_server.m
+++ src/tcp_server.m
@@ -19,9 +19,11 @@ a=1;
 b=2;
 c=3;
 while(true)
+        
         data=fgetl(t);
+       
         vector=str2num(data);
-        if(length(vector)>5)
+        if(length(vector)>6)
           %if(vector(1)==1)
            % old_vector1=vector;
           %else
@@ -37,27 +39,17 @@ while(true)
              % old_vector2=vector;
             %end
           %end
-            axis([-100 100 -100 100 -100 100])
-            quiver3(0,0,0,vector(1), vector(2),vector(3))
-            if (vector(1)>10000)
-                a
-                vector(1)
-            end
-            if (vector(2)>10000)
-                b
-                vector(2)
-            end
-            if (vector(3)>10000)
-                c
-                vector(3)
-            end
-            axis([-100 100 -100 100 -100 100])
+            axis([-2000 2000 -2000 2000 -2000 2000])
+            quiver3(0,0,0,vector(2), vector(3),vector(4))
+            
+            axis([-2000 2000 -2000 2000 -2000 2000])
             hold on
-            quiver3(0,0,0,vector(4), vector(5),vector(6))
+            quiver3(0,0,0,vector(5), vector(6),vector(7))
             hold off
             legend('sensor thumb', 'sensor pinky');
+            
        end
-       flushinput(t);
+        flushinput(t);
        pause(0.05);
 %end
 end
